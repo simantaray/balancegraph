957628c1aa2fe0179a59902e9e99852e543937f7
simantaray <simantaray89@gmail.com>
2022-03-29 19:42:19 +0530

    code-complete

--- src/Components/Monthly.js
+++ src/Components/Monthly.js
@@ -2,26 +2,11 @@ import React, { useState } from "react";
 import Graph from "./Graph"
 export default function Monthly({initialBalance}) {
   const [monthly, setMonthly] = useState(1);
-    const[data,setData]=useState();
-  const array=[];
   
  
   const handleSubmit = (event) => {
     event.preventDefault();
-   // setArray(oldArray => [...oldArray, 10]);
-
-    
-    while(1<=initialBalance){
-       
-        
-         initialBalance=initialBalance-monthly;
-         console.log(initialBalance)
-        //  setData(initialBalance)
-         array.push(initialBalance)
-   
-    }
-    setData(array)
-    
+  
   };
   
   return (
