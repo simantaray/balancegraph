3406fbeba05a08b04ccc5b6945a4c9e55d364fca
simantaray <simantaray89@gmail.com>
2022-03-29 20:05:41 +0530

    fixed : removing console.logs

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -6,30 +6,42 @@ import { Line } from "react-chartjs-2";
 ChartJS.register(...registerables);
 
 export default function App({ initialBalance, monthly }) {
-  
   let array = [];
   while (1 <= initialBalance) {
     array.push(initialBalance);
     initialBalance = initialBalance - monthly;
-    console.log(initialBalance);
-    
   }
-  console.log(array);
+
 
   return (
     <div className="graph1">
-      
-      <Line className="graph2" data={{
-    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-    datasets: [
-      {
-        label: "Balance monthwise",
-        data: array,
-        fill: false,
-        borderColor: "#22A7F0",
-      },
-    ],
-  }} />
+      <Line
+        className="graph2"
+        data={{
+          labels: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+          ],
+          datasets: [
+            {
+              label: "Balance monthwise",
+              data: array,
+              fill: false,
+              borderColor: "#22A7F0",
+            },
+          ],
+        }}
+      />
     </div>
   );
 }
